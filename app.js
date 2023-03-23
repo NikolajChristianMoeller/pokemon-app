4fdb960cab3e6bd95710d932489b518ce2a4730f
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-03-23 09:02:51 +0100

    Ændringer
    Ændringer

--- app.js
+++ app.js
@@ -29,52 +29,50 @@ function initApp() {
     statsSpeed: "110",
   };
   console.log(gengar);
-  showPokémon(gengar);
+  showPokemon(gengar);
 
   document.querySelector("#dialog");
 
 }
 
-// function showPokémon(pokemon) {
-//   const myHTML = /*html*/ `
-//   <li>Name: ${pokemon.name}</li>
-//   <li>Description: ${pokemon.description}</li>
-//   <li>Ability: ${pokemon.ability}</li>
-//   <li>Image: ${pokemon.image}</li>
-//   <li>Footprint: ${pokemon.footprint}</li>
-//   <li>Dexindex: ${pokemon.dexindex}</li>
-//   <li>Type: ${pokemon.type}</li>
-//   <li>Subtype: ${pokemon.subtype}</li>
-//   <li>Weaknesses: ${pokemon.weaknesses}</li>
-//   <li>Gender: ${pokemon.gender}</li>
-//   <li>Weight: ${pokemon.weight}</li>
-//   <li>Height: ${pokemon.height}</li>
-//   <li>Generation: ${pokemon.generation}</li>
-//   <li>Spilversion: ${pokemon.spilversion}</li>
-//   <li>CanEvole: ${pokemon.canEvolve}</li>
-//   <li>StatsHP: ${pokemon.statsHP}</li>
-//   <li>StatsAttack: ${pokemon.statsAttack}</li>
-//   <li>StatsDefence: ${pokemon.statsDefence}</li>
-//   <li>StatsSpecialAttack: ${pokemon.statsSpecialAttack}</li>
-//   <li>StatsSpecialDefence: ${pokemon.statsSpecialDefence}</li>
-//   <li>StatsSpeed: ${pokemon.statsSpeed}</li> `
-//   document.querySelector("#pokémon").insertAdjacentHTML("beforeend", myHTML);
-// }
+function showPokemonDialog(pokemon) {
+  const myHTML = /*html*/ `
+  <li>Name: ${pokemon.name}</li>
+  <li>Description: ${pokemon.description}</li>
+  <li>Ability: ${pokemon.ability}</li>
+  <li>Image: ${pokemon.image}</li>
+  <li>Footprint: ${pokemon.footprint}</li>
+  <li>Dexindex: ${pokemon.dexindex}</li>
+  <li>Type: ${pokemon.type}</li>
+  <li>Subtype: ${pokemon.subtype}</li>
+  <li>Weaknesses: ${pokemon.weaknesses}</li>
+  <li>Gender: ${pokemon.gender}</li>
+  <li>Weight: ${pokemon.weight}</li>
+  <li>Height: ${pokemon.height}</li>
+  <li>Generation: ${pokemon.generation}</li>
+  <li>Spilversion: ${pokemon.spilversion}</li>
+  <li>CanEvole: ${pokemon.canEvolve}</li>
+  <li>StatsHP: ${pokemon.statsHP}</li>
+  <li>StatsAttack: ${pokemon.statsAttack}</li>
+  <li>StatsDefence: ${pokemon.statsDefence}</li>
+  <li>StatsSpecialAttack: ${pokemon.statsSpecialAttack}</li>
+  <li>StatsSpecialDefence: ${pokemon.statsSpecialDefence}</li>
+  <li>StatsSpeed: ${pokemon.statsSpeed}</li> `
+  document.querySelector("#pokemon").insertAdjacentHTML("beforeend", myHTML);
+}
 
 function showPokemon(pokemon) {
   console.log(pokemon);
-  document.querySelector("#pokemons").insertAdjacentHTML(
-    "beforeend",
+  document.querySelector("#pokemons").insertAdjacentHTML("beforeend", myHTML);
+  document.querySelector("#pokemons article:last-child").addEventListener("click", pokemonClicked);
     /*html*/ `
       <article class="grid-item">
         <img src="${pokemon.image}">
         <h2>${pokemon.name}</h2>
-        <p>${pokemon.type}</p>
+        <p>Type ${pokemon.type}, ${pokemon.subtype}</p>
         <p>Pokedex Number: ${pokemon.dexindex}</p>
       </article>
-    `
-  );
-  document.querySelector("#pokemons article:last-child").addEventListener("click", pokemonClicked);
+    `;
 
   function pokemonClicked() {
     console.log(pokemon);
@@ -88,6 +86,13 @@ function showPokemon(pokemon) {
     document.querySelector("#dialog-pokemon").showModal();
   }
 }
+
+showPokemon(gengar);
+showPokemon(gengar);
+showPokemon(gengar);
+showPokemon(gengar);
+showPokemon(gengar);
+showPokemon(gengar);
 
 function fetchJSON() {}
 
@@ -125,51 +130,3 @@ function getDescription () {}
 //     // append article to grid
 //     document.querySelector("#characters").appendChild(articleElement);
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="#" type="image/x-icon">
-    <link rel="shortcut icon" href="https://cederdorff.github.io/img/favicon.webp" type="image/x-icon">
-    <link rel="stylesheet" href="app.css">
-    <title>Pokémon App</title>
-</head>
-<body>
-    <header>
-        <h1>Pokémon Characters</h1>
-    </header>
-    <dialog id="dialog">
-        <h2 id="dialog-pokemon-name">Firstname</h2>
-		<h3 id="dialog-type">This is my type!</h3>
-		<p id="dialog-dexindex">This is my dexindex...</p>
-		<img id="dialog-image" src="https://cederdorff.github.io/img/favicon.webp">
-        <form method="dialog">
-		<button id="btn-cancel">Close</button>
-        </form>
-        <div id="pokemon">
-        </div>
-    </dialog>
-    <main>
-        <section id="pokemons" class="grid-container"></section>
-    </main>
-    <footer>
-        <p>Pokémon App by Nikolaj</p>
-    </footer>
-
-    <script src="app.js"></script>
-</body>
-</html>
